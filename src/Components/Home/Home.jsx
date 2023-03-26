e8ca98d09ede62e980799b9734ee7db7af9c15f6
Karan Sharma <sharma.karan27oct@gmail.com>
2023-03-26 14:05:14 +0530

    header completed

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Container, Heading } from '@chakra-ui/react'
+import React from 'react'
+import './Home.css'
+const Home = () => {
+  return (
+    <div className='landingPage'>
+    <Container>
+        <Heading color={'white'} >Hi</Heading>
+    </Container>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Components/Layout/Header/Header.jsx
@@ -0,0 +1,142 @@
+import {
+  Button,
+  Container,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import React from 'react';
+// import { ColorModeSwitcher } from '../../../ColorModeSwitcher';
+import { RiMenu5Fill, RiTeamLine, RiRegisteredLine } from 'react-icons/ri';
+import { GiSmartphone } from 'react-icons/gi';
+import { GrDocumentConfig, GrGallery } from 'react-icons/gr';
+import { AiOutlineHome } from 'react-icons/ai';
+import { BsCardChecklist } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { FaChevronDown, FaRegHandshake } from 'react-icons/fa';
+import logo from '../../../Assets/Logos/black.png';
+const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      {/* <ColorModeSwitcher /> */}
+      <Button
+        colorScheme={'red'}
+        position="fixed"
+        top={'6'}
+        left="6"
+        width="12"
+        height="12"
+        rounded="full"
+        onClick={onOpen}
+        zIndex="1000"
+      >
+        <RiMenu5Fill />
+      </Button>
+      <Drawer placement="left" isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay backdropFilter={'blur(10px)'} />
+        <DrawerContent>
+          <DrawerHeader color={'crimson'} borderBottomWidth={'2px'} >
+            <Container justifyContent="center" alignItems={'center'}>
+              <Image src={logo} width={'250px'} alt="Dan Abramov" />
+            </Container>
+          </DrawerHeader>
+          <DrawerBody>
+            <VStack spacing={'4'} alignItems="flex-start">
+              <Link onClick={onClose} to="/">
+                <Button
+                  variant="ghost"
+                  leftIcon={<AiOutlineHome fontSize={'18px'} />}
+                >
+                  {' '}
+                  Home
+                </Button>
+              </Link>
+              <Menu>
+                <MenuButton
+                  variant="ghost"
+                  as={Button}
+                  rightIcon={<FaChevronDown />}
+                  leftIcon={<GrDocumentConfig />}
+                >
+                  About
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>
+                    <Link onClick={onClose} to="/request">
+                      <Button variant="ghost">MUN Conference</Button>
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link onClick={onClose} to="/request">
+                      <Button variant="ghost">Zero Hour Summit</Button>
+                    </Link>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+              <Link onClick={onClose} to="/request">
+                <Button
+                  variant="ghost"
+                  leftIcon={<RiRegisteredLine fontSize="18px" />}
+                >
+                  Registrations
+                </Button>
+              </Link>
+              <Link onClick={onClose} to="/contact">
+                <Button
+                  variant="ghost"
+                  leftIcon={<BsCardChecklist fontSize="18px" />}
+                >
+                  Agendas
+                </Button>
+              </Link>
+              <Link onClick={onClose} to="/about">
+                <Button
+                  variant="ghost"
+                  leftIcon={<GrGallery fontSize="18px" />}
+                >
+                  Gallery
+                </Button>
+              </Link>
+              <Link onClick={onClose} to="/about">
+                <Button
+                  variant="ghost"
+                  leftIcon={<FaRegHandshake fontSize="18px" />}
+                >
+                  Sponsers
+                </Button>
+              </Link>
+              <Link onClick={onClose} to="/about">
+                <Button
+                  variant="ghost"
+                  leftIcon={<GiSmartphone fontSize="18px" />}
+                >
+                  Contact Us
+                </Button>
+              </Link>
+              <Link onClick={onClose} to="/about">
+                <Button
+                  variant="ghost"
+                  leftIcon={<RiTeamLine fontSize={'18px'} />}
+                >
+                  Team
+                </Button>
+              </Link>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Header;
